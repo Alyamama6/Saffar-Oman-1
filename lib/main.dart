b60d39618ebd289a86ef9767e0c8da63868203a8
Alyamama6 <alyadvweb@gmail.com>
2026-05-16 22:18:54 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
 // Screens
 import 'Welcome.dart';
 import 'accounttype.dart';
